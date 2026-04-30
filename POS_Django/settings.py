2e41e5638b44954325bf329cc0fb81db9a547811
Jose Ortiz <joseortizmartinez62@gmail.com>
2026-04-30 15:31:36 -0600

    Migracion a PostgreSQL

--- POS_Django/settings.py
+++ POS_Django/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'POS_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pos_db',
+        'USER': 'pos_user',
+        'PASSWORD': 'Marzo.2004',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
